4fd18c5e5a95500008fd9bb7dd713ac8df2bb554
Sailinkan Mohanty <75167775+sailinkan2000@users.noreply.github.com>
2023-05-29 12:40:08 +0530

    Delete AddProduct directory

--- AddProduct/bin/src/main/webapp/index.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Add Product</h2>
-<a href="pets">List Pets</a> <br>
-<a href="addpet.jsp">Add New Pet</a><br>
-</body>
-</html>
--- AddProduct/src/main/webapp/addproduct.js
+++
@@ -1,9 +0,0 @@
-/**
- * Addproduct.js Javascript
- */
-
-function addpet_onclick() {
-	//alert("hi");
-	var x = document.getElementsByClassName("form_class");
-	x[0].submit(); //form submission
-}	
