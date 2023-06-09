577b3211760f1198aaff8f586ab29e3c72d73ab0
Sailinkan Mohanty <75167775+sailinkan2000@users.noreply.github.com>
2023-06-09 19:30:57 +0530

    Add files via upload

---
+++ SpringBootProject-master/src/main/java/com/controller/FeedbackController.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.service.AppService;
+
+@Controller
+public class FeedbackController {
+	
+	@Autowired
+	private AppService appService;
+	
+	@GetMapping("/feedback")
+	public String feedback() {
+		return "feedback";
+	}
+
+	public AppService getAppService() {
+		return appService;
+	}
+
+	public void setAppService(AppService appService) {
+		this.appService = appService;
+	}
+}
